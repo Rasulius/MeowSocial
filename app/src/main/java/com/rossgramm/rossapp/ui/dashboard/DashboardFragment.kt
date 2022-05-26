799844c2a1fa8e81922e59dbb954ca9eb8fd2e83
Rasul <hotgang@mail.ru>
2022-05-26 21:35:43 +0500

    feat: вернул

--- app/src/main/java/com/rossgramm/rossapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/rossgramm/rossapp/ui/dashboard/DashboardFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.rossgramm.rossapp.dashboard.data.AlbumItem
 import com.rossgramm.rossapp.databinding.FragmentDashboardBinding
-import com.rossgramm.rossapp.login.data.adapters.AlbumAdapter
 
 class DashboardFragment : Fragment() {
 
@@ -21,13 +17,22 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return binding.root
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
     }
 
     override fun onDestroyView() {
